e6188caae33173e2a9dc84731dfbb720f3b6e193
coinkeeperadmin <eogus777@protonmail.com>
2025-11-02 18:33:42 +0000

    Create embed.js

---
+++ docs/Functions/embed.js
@@ -0,0 +1,2 @@
+export const onRequestGet = () =>
+  Response.redirect('https://widget.pegasusswap.com/widget?deposit=USDT&depositNetwork=TRX&receive=TRX&receiveNetwork=TRX&amount=&isOptional=false&ref=URNNCCO', 302)
